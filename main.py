529bb10098afec5e7238f6bbfe699813b8533b22
William Freeman <wfreeman@fmgl.com.au>
2021-12-30 18:18:24 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+import os
+
+
+#TOKEN = os.getenv('TEST_TOKEN')
+TOKEN = 'ODEzMjk2ODU1ODE1MjkwOTMw.YDNPuA.ZsS_uLrwrQrP4urHhktX3_VPyrY'
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    #channel = client.get_channel(812906949124423700)
+    if message.author == client.user:
+        return
+    else:
+        await message.channel.send('Heya')
+
+client.run(TOKEN)
